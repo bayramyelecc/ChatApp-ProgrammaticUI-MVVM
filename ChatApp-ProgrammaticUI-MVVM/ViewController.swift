7f4a7560f05c3a3cee7a998333219facc0f35263
Bayram Yelec <bayrammyelec@gmail.com>
2024-11-12 16:18:47 +0300

    Initial Commit

---
+++ ChatApp-ProgrammaticUI-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp-ProgrammaticUI-MVVM
+//
+//  Created by Bayram Yeleç on 12.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
